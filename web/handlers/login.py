3301d992fa3c025d3e1ae50dea6f1d646957b636
cdpidan <jks_pidan@foxmail.com>
2018-06-26 15:39:43 +0800

    找回密码邮件标题

--- web/handlers/login.py
+++ web/handlers/login.py
@@ -227,7 +227,7 @@ class PasswordResetHandler(BaseHandler):
         verified_code = self.db.user.encrypt(0, [user['id'], verified_code])
         verified_code = base64.b64encode(verified_code)
 
-        future = utils.send_mail(to=user['email'], subject=u"签到(qiandao.today) 密码重置", html=u"""
+        future = utils.send_mail(to=user['email'], subject=u"签到(%s) 密码重置" % (config.domain), html=u"""
 
         <h1 style="margin-left: 30px;">签到<sup>alpha</sup></h1>
 
